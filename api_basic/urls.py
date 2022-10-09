5bba824d216237342f9a6118ce798006d6eb1ebd
Bektur Tologonov <tbektur7@gmail.com>
2022-10-09 17:14:15 +0600

    updated

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('article/', article_list),
-    path('detail/<int:pk>/', article_detail)
-
+    # path('article/', article_list),
+    path('article/', ArticleAPIView.as_view()),
+    # path('detail/<int:pk>/', article_detail)
+    path('detail/<int:id>', ArticleDetails.as_view()),
+    path('generic/article/<int:pk>/', GenericAPIView.as_view())
 ]
